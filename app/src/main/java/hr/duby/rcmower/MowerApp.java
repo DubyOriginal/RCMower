e42dc2a88885abc0b685e18bee375287820226bf
DubyOriginal <DubyOriginal@gmail.com>
2016-11-06 10:47:48 +0100

    InfoActivity - sensor readings.... in progress 02

---
+++ app/src/main/java/hr/duby/rcmower/MowerApp.java
@@ -0,0 +1,21 @@
+package hr.duby.rcmower;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by Duby on 6.11.2016..
+ */
+
+public class MowerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("DTag", "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Log.d("DTag", "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Log.d("DTag", "STATUS: VERSION_NAME: " + BuildConfig.VERSION_NAME + ", DEBUG: " + BuildConfig.DEBUG);
+    }
+}
+
+
