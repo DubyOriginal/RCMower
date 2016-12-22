38be479ec81d3fa3b8934ec17a261722ee7f9d9b
DubyOriginal <DubyOriginal@gmail.com>
2016-12-22 18:15:01 +0100

    SocketTestActivity.... 02

--- app/src/main/java/hr/duby/rcmower/activities/SocketTestActivity.java
+++ app/src/main/java/hr/duby/rcmower/activities/SocketTestActivity.java
@@ -22,26 +22,26 @@ import java.util.ArrayList;
 import hr.duby.rcmower.R;
 
 public class SocketTestActivity extends AppCompatActivity implements Runnable, View.OnClickListener, AdapterView.OnItemClickListener {
-    /**
-     * Called when the activity is first created.
-     */
 
     final String SERVER_IP = "192.168.4.1";
     final int SERVER_PORT = 81;
 
-    final static int TIME_OUT = 5000;
     final static byte NM_SEND_OS_TYPE = 1;
     final static byte NM_SEND_MESSAGE = 2;
+    final static int TIME_OUT = 5000;
+    final static int SEND_BUFFER_SIZE = 2;      //def 1024
+    final static int RECEIVE_BUFFER_SIZE = 2;   //def 1024
 
 
     private Socket m_client_socket = null;
     private BufferedOutputStream m_out_stream = null;
     private InputStream m_in_stream = null;
-
-
     private Thread m_client_thread;
 
+    //VARS
     private ArrayAdapter<String> m_adapter;
+    private String m_recv_string;
+    private String m_debug_string;
 
     //WIDGETS
     private ListView m_list_view;
@@ -49,10 +49,7 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
     private EditText etInputMsg;
     private Button btnSendMsg, btnPumpOn, btnPumpOff;
 
-    private String m_recv_string;
-    private String m_debug_string;
 
-    //연결 상태 표시용 쓰레드
     Runnable m_debug_run = new Runnable() {
         public void run() {
             m_text_view.setText(m_debug_string);
@@ -70,7 +67,6 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
 
             etInputMsg.setText("");
         }
-
     };
 
 
@@ -88,7 +84,7 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
         m_list_view.setOnItemClickListener(this);
 
         m_text_view = (TextView) findViewById(R.id.id_tv);
-        m_text_view.setText("tring connect!");
+        m_text_view.setText("NOT Connected!");
 
         etInputMsg = (EditText) findViewById(R.id.etInputMsg_sta);
 
@@ -108,7 +104,7 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
     @Override
     //**********************************************************************************************
     protected void onStop() {
-
+        DLog("onStop");
         if (m_client_socket != null) {
             try {
 
@@ -132,15 +128,17 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
     //**********************************************************************************************
     public void run() {
 
+        DLog("run - T32");
+
         try {
             SocketAddress sock_addr = new InetSocketAddress(SERVER_IP, SERVER_PORT);
             m_client_socket = new Socket();
-            m_client_socket.setReceiveBufferSize(1024);
-            m_client_socket.setSendBufferSize(1024);
+            m_client_socket.setReceiveBufferSize(RECEIVE_BUFFER_SIZE);
+            m_client_socket.setSendBufferSize(SEND_BUFFER_SIZE);
             m_client_socket.setSoLinger(true, TIME_OUT);
-            m_client_socket.setSoTimeout(1000 * 60 * 15);
+            m_client_socket.setSoTimeout(1000 * 60 * 15);   //15min
             m_client_socket.connect(sock_addr, TIME_OUT);
-            m_debug_string = "Connect Server!!";
+            m_debug_string = "Server Connected!!";
             m_text_view.post(m_debug_run);
 
             m_out_stream = new BufferedOutputStream(m_client_socket.getOutputStream());
@@ -166,12 +164,6 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
             int s = ((size[1] << 8) & 0xff00) + (size[0] & 0x00ff);
             DLog("------result------> " + s);
 
-
-            // #O   open
-            // #C   close
-            // #S   Get current status
-            // #T   Obtaining Temperature
-            //  m_out_stream.write(NM_SEND_OS_TYPE);
             m_out_stream.write(size);
             m_out_stream.write(data);
             m_out_stream.flush();
@@ -179,7 +171,6 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
             onReadStream();
 
         } catch (Exception e) {
-
             m_debug_string = e.getMessage();
             m_text_view.post(m_debug_run);
 
@@ -206,10 +197,10 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
 
     //***********************************************************************************************************************************
     public void onReadStream() throws IOException {
+        DLog("onReadStream");
 
         byte msg_id;
         byte[] size = new byte[2];
-
 
         while (!m_client_thread.isInterrupted()) {
 
@@ -257,37 +248,29 @@ public class SocketTestActivity extends AppCompatActivity implements Runnable, V
                     cmd_buffer = "wf";
                 }else if (view.getId() == R.id.btnSendMsg_sta){
                     cmd_buffer = etInputMsg.getText().toString();
-                    //byte[] data = cmd_buffer.getBytes();
-                    //data_size = data.length;
-
-                    // Stores the size of the byte. Because Linux and Windows, which are Android's bases, have different Byte Ordering
-                    // When sending / receiving 2 bytes of data, you have to change the value by 1 byte.
-                    //byte[] size = new byte[2];
-                    //size[0] = (byte)data_size;
-                    //size[1] = (byte)(data_size >> 8);
                 }
 
-                DLog("CMD: " +  cmd_buffer);
-
                 data_size = cmd_buffer.length();
-                DLog("CMD_size: " + data_size);
 
+                // Stores the size of the byte. Because Linux and Windows, which are Android's bases, have different Byte Ordering
+                // When sending / receiving 2 bytes of data, you have to change the value by 1 byte.
                 byte[] data = cmd_buffer.getBytes();
 
                 byte[] size = new byte[2];
-                size[0] = (byte) (data_size & 0x00ff);   //1을 넣고
-                size[1] = (byte) ((data_size & 0xff00) >> 8);       //0으로 채운다.
+                size[0] = (byte) (data_size & 0x00ff);
+                size[1] = (byte) ((data_size & 0xff00) >> 8);
 
                 int s = ((size[1] << 8) & 0xff00) + (size[0] & 0x00ff);
-                DLog("result: " + s);
+                DLog("CMD: '" +  cmd_buffer + "', size: " + data_size + ", result: " + s);
 
                 m_out_stream.write(size);   // Write size information.
                 m_out_stream.write(data);   //Write a byte array.
                 m_out_stream.flush(); // It sends the information written to the stream to the server.
 
-
             } catch (IOException e) {
                 m_text_view.setText(e.getMessage());
+                DLog("IOException - ERR4595 - " + e);
+
             }
         } else {
             DLog("Not connected or Socked Closed!");
